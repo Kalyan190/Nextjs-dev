3a2550094be178c9292b0d4118f23b0ec88d65db
Kalyan Kumar <kalyaniit795@gmail.com>
2025-02-25 00:40:11 +0530

    hero page change

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -6,7 +6,7 @@ import { FaStar } from 'react-icons/fa'
 const Hero = () => {
   return (
      <div className="relative w-full h-[110vh] sm:h-screen bg-[url('/images/bg.png')] bg-cover bg-center flex justify-center flex-col ">
-      <div className='w-[90%] md:w-[80%] mx-auto items-center grid grid-cols-1 xl:grid-cols-2 gap-2 '>
+      <div className='w-[90%] md:w-[80%] mx-auto items-center grid grid-cols-1 xl:grid-cols-2 gap-2 mt-16 sm:mt-0'>
            <div data-aos="fade-up">
               <p className='text-sm sm:text-base md:text-xl font-bold text-blue-950'>Make the Smartest Investment</p>
               <h1 className='text-2xl md:text-3xl lg:text-4xl mt-6 mb-6  font-bold text-blue-950 leading-[2.5rem] md:leading-[3.5rem] '>Kickstart, Scale, and Thrive Faster with <span className='text-rose-700'>TechDev</span></h1>
